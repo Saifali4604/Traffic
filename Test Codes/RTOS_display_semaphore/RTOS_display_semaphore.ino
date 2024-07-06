608a68745bd6efd7dc5df379bf4e78eca8a5a8bf
Saif Ali <msaifalishaikh.ec22@rvce.edu.in>
2024-07-06 12:09:40 +0530

    Update RTOS_display_semaphore.ino

--- Test Codes/RTOS_display_semaphore/RTOS_display_semaphore.ino
+++ Test Codes/RTOS_display_semaphore/RTOS_display_semaphore.ino
@@ -4,21 +4,12 @@
 #include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <SPI.h>             // Arduino SPI library
 
-#define TFT_CS  2
+#define TFT_CS  13
 #define TFT_RST 5
 #define TFT_DC 4
 #define I2S_WS 15    // I2S word select pin
 #define I2S_SCK 14   // I2S clock pin
 #define I2S_SD 32    // I2S data pin
-
-#define green1 27
-#define green2 26
-#define green3 34
-#define green4 16
-#define yellow1 33
-#define yellow2 19
-#define yellow3 21
-#define yellow4 22
 
 // I2S configuration
 const i2s_config_t i2s_config = {
@@ -53,13 +44,7 @@ int16_t i2sData[1024];
 int32_t sum = 0;
 int R = 1;
 int Traffic_delay = 1000;
-int sensitivity = 70; // Sensitivity variable (0 to 100)
-
-void Traffic_light(void *pvParameters); 
-TaskHandle_t Traffic_light_handler; 
-
-void Traffic_time_control(void *pvParameters); 
-TaskHandle_t Traffic_time_control_handler; 
+int sensitivity = 50; // Sensitivity variable (0 to 100)
 
 void Traffic_display_airquality(void *pvParameters); 
 TaskHandle_t Traffic_display_airquality_handler; 
@@ -74,14 +59,6 @@ void setup(){
   i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
   i2s_set_pin(I2S_NUM_0, &pin_config);
   i2s_set_clk(I2S_NUM_0, 44100, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO);
-  pinMode(green1,OUTPUT);
-  pinMode(green2,OUTPUT);
-  pinMode(green3,OUTPUT);
-  pinMode(green4,OUTPUT);
-  pinMode(yellow1,OUTPUT);
-  pinMode(yellow2,OUTPUT);
-  pinMode(yellow3,OUTPUT);
-  pinMode(yellow4,OUTPUT);
 
   tft.init(240, 240, SPI_MODE2);    // Init ST7789 display 240x240 pixels
   tft.setRotation(3);
@@ -96,12 +73,9 @@ void setup(){
   tftMutex = xSemaphoreCreateMutex();
 
   Traffic_lights_time_signal = xEventGroupCreate(); 
-  xTaskCreate(Traffic_light, "Blink Led", 2048, NULL, 1 , &Traffic_light_handler);
+
   xTaskCreate(Traffic_display_airquality, "display", 2048, NULL, 1 , &Traffic_display_airquality_handler);
   xTaskCreate(Traffic_display_noise, "display", 2048, NULL, 1 , &Traffic_display_noise_handler);
-  xTaskCreate(Traffic_time_control, "Blink Led", 2048, NULL, 1 , &Traffic_time_control_handler);
-  xEventGroupSetBits(Traffic_lights_time_signal, 1); 
-  xEventGroupSetBits(Traffic_lights_time_signal, 0);
 } 
 
 void displayAirQuality(int value) {
@@ -196,88 +170,7 @@ void Traffic_display_noise(void *pvParameters){
   }
 }
 
-void Traffic_time_control(void *pvParameters){  
-  for(;;){
-    EventBits_t xBits = xEventGroupWaitBits( 
-                 Traffic_lights_time_signal,    // The event group being tested. 
-                 1 | 2 | 3 | 4,  // The bits within the event group to wait for. 
-                 pdTRUE,         // BIT_0 and BIT_4 should be cleared before returning. 
-                 pdTRUE,        // Don't wait for both bits, either bit will do. 
-                 portMAX_DELAY);  //ms 
-    int r = xBits;
-    // fetch the No. of cars 
-    // calculate the time for r road by comparing with others roads
-    // max time should be standard time.
-    EventBits_t xBit = xEventGroupWaitBits( 
-                 Traffic_lights_time_signal,    // The event group being tested. 
-                 0,  // The bits within the event group to wait for. 
-                 pdTRUE,         // BIT_0 and BIT_4 should be cleared before returning. 
-                 pdFALSE,        // Don't wait for both bits, either bit will do. 
-                 portMAX_DELAY);  //ms 
-    // load the time Traffic_delay
-    xEventGroupSetBits(Traffic_lights_time_signal, 5);
-  }
-}
 
-void Traffic_light(void *pvParameters) {
-  for (;;) {
-    EventBits_t xBits = xEventGroupWaitBits(
-      Traffic_lights_time_signal,    // The event group being tested.
-      5,  // The bits within the event group to wait for.
-      pdTRUE,         // Clear bits before returning.
-      pdFALSE,        // Wait for any bit.
-      portMAX_DELAY); // Wait indefinitely
-
-    switch (R) {
-      case 1:
-        digitalWrite(green1, HIGH);
-        xEventGroupSetBits(Traffic_lights_time_signal, 2);
-        vTaskDelay(pdMS_TO_TICKS(Traffic_delay/2));
-        digitalWrite(green1, LOW);
-        digitalWrite(yellow1, HIGH);
-        vTaskDelay(pdMS_TO_TICKS(Traffic_delay/2));
-        xEventGroupSetBits(Traffic_lights_time_signal, 0);
-        digitalWrite(yellow1, LOW);
-        R = 2;
-        break;
-      case 2:
-        digitalWrite(green2, HIGH);
-        xEventGroupSetBits(Traffic_lights_time_signal, 3);
-        vTaskDelay(pdMS_TO_TICKS(Traffic_delay/2));
-        digitalWrite(green2, LOW);
-        digitalWrite(yellow2, HIGH);
-        vTaskDelay(pdMS_TO_TICKS(Traffic_delay/2));
-        xEventGroupSetBits(Traffic_lights_time_signal, 0);
-        digitalWrite(yellow2, LOW);
-        R = 3;
-        break;
-      case 3:
-        digitalWrite(green3, HIGH);
-        xEventGroupSetBits(Traffic_lights_time_signal, 4);
-        vTaskDelay(pdMS_TO_TICKS(Traffic_delay/2));
-        digitalWrite(green3, LOW);
-        digitalWrite(yellow3, HIGH);
-        vTaskDelay(pdMS_TO_TICKS(Traffic_delay/2));
-        xEventGroupSetBits(Traffic_lights_time_signal, 0);
-        digitalWrite(yellow3, LOW);
-        R = 4;
-        break;
-      case 4:
-        digitalWrite(green4, HIGH);
-        xEventGroupSetBits(Traffic_lights_time_signal, 1);
-        vTaskDelay(pdMS_TO_TICKS(Traffic_delay/2));
-        digitalWrite(green4, LOW);
-        digitalWrite(yellow4, HIGH);
-        vTaskDelay(pdMS_TO_TICKS(Traffic_delay/2));
-        xEventGroupSetBits(Traffic_lights_time_signal, 0);
-        digitalWrite(yellow4, LOW);
-        R = 1;
-        break;
-      default:
-        break;
-    }
-  }
-}
 
 void loop(){} 
 
